8d99aa3e45c52b32832873d27f379a522142714c
OldSkool <oldskoolstreamalt@gmail.com>
2025-02-01 14:54:40 -0500

    moved recognizer initialization into constructor

--- brianator/js/stt.mjs
+++ brianator/js/stt.mjs
@@ -1,12 +1,30 @@
-const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 
 class SpeechToText {
     constructor() {
+        const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
         const rec =  new SpeechRecognition();
         rec.continuous = true;
-        rec.interimResults = false;
+        rec.interimResults = true;
         this._recognizer = rec;
     }
+
+    start() {
+
+    }
+
+    _handleResult(evt) {
+
+    }
+
+    onFinalResult(result) {
+        return false; // Overridden by consumer
+    }
+
+    onInterimResult(result) {
+        return false; // Overridden by consumer
+    }
 }
+
+
 
 export { SpeechToText };
