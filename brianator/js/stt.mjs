c74fab7a67d522207644ca7facb30d826ec80143
OldSkool <oldskoolstreamalt@gmail.com>
2025-02-01 18:25:18 -0500

    Working version of BRIANator 5000

--- brianator/js/stt.mjs
+++ brianator/js/stt.mjs
@@ -1,19 +1,43 @@
 
 class SpeechToText {
     constructor() {
-        const SpeechRecognition = SpeechRecognition ?? webkitSpeechRecognition;
-        const rec =  new SpeechRecognition();
+        const rec =  new webkitSpeechRecognition();
+        this._recognizer = rec;
         rec.continuous = true;
         rec.interimResults = true;
-        this._recognizer = rec;
+        rec.lang = 'en-US';
+        rec.onresult = (evt) => {
+            return this._handleResult(evt);
+        }
+        rec.onspeechend = () => {
+            console.debug('stt: speechend()');
+            this.stop();
+        }
     }
 
     start() {
+        console.debug('stt: start()');
+        this._recognizer.start();
+    }
 
+    stop() {
+        this._recognizer.stop();
+        this.onEnd();
     }
 
     _handleResult(evt) {
+        const resultIndex = evt.results.length - 1;
+        const result = evt.results[resultIndex];
+        const text = result[0]?.transcript;
+        if (result.isFinal) {
+            return this.onFinalResult(resultIndex, text);
+        } else {
+            return this.onInterimResult(resultIndex, text);
+        }
+    }
 
+    onEnd() {
+        return false; // Overridden by consumer
     }
 
     onFinalResult(result) {
--- brianator/js/view.mjs
+++ brianator/js/view.mjs
@@ -6,60 +6,82 @@ class BrianatorView {
         this._audioSrcElement = opts?.audioSrc;
         this._control = opts?.control;
         this._log = opts?.log;
-        this._playlingLineId = null;
-        this._recordingLineId = null;
+        this._logContainer = opts?.logContainer;
+        this._playingLineId = null;
+        this._recordingId = null;
         this._stt = new SpeechToText();
-        this._lines = [];
+        this._recordings = [];
         this._outputQueue = [];
     }
 
     initialize() {
         this._audioElement.addEventListener('ended', () => {
+            const line = document.getElementById(this._playingLineId);
+            line.classList.remove('playing');
+            this._playingLineId = null;
             this._playNextLine();
         });
 
         this._control.addEventListener('click', () => { 
-            if (this._recordingLineId) {
+            if (this._recordingId) {
                 this._stt.stop();
             } else {
-                this._recordingLineId = this._getNewLineId();
+                this._recordingId = `recording-${this._getNewRecordingId()}`;
                 const div = document.createElement('DIV');
-                div.id = `line-${this._recordingLineId}`;
-                div.classList.add('recording');
-                this._control.classList.add("recording");
+                div.id = this._recordingId;
+                div.classList.add('recording', 'live');
+                this._recordings.push(div);
+                this._log.appendChild(div);
+                this._control.classList.add("live");
                 this._stt.start();
+                this._logContainer.scrollTo({top: 0, behavior: 'smooth'});
             }
         });
 
-        this._stt.onFinalResult((result) => {
-            this._queueLine(this._recordingLineId, result);
-        });
+        this._stt.onFinalResult = (resultNumber, text) => {
+            const line = this._getOrMakeLine(this._recordingId, resultNumber);
+            if (!line) return;
+            line.innerText = text;
+            line.classList.add('final');
+            console.debug(`final result: ${text}`);
+            this._queueLine(line.id);
+        };
 
-        this._stt.onInterimResult((result) => {
-            const line = this._lines.find(l => l.id == this._recordingLineId);
-            line.text = result;
+        this._stt.onInterimResult = (resultNumber, text) => {
+            const line = this._getOrMakeLine(this._recordingId, resultNumber);
+            if (!line) return;
+            line.innerText = text;
+            console.debug(`interim result: ${text}`);
+        };
 
-        });
-
-        this._stt.onEnd(() => {
-            const line = this._lines.find(l => l.id == this._recordingLineId);
-            const div = document.getElementById(`line-${line?.id}`);
-            div?.classList.remove('recording');
-            this._recordingLineId = null;
-            this._control.classList.remove("recording");
-        });
+        this._stt.onEnd = () => {
+            const recording = this._recordings.find(l => l.id == this._recordingId);
+            recording?.classList.remove('live');
+            this._recordingId = null;
+            this._control.classList.remove("live");
+        };
     }
 
-    _getNewLineId() {
-        if (crypto.randomUUID) {
-            return crypto.randomUUID();
+    _getNewRecordingId() {
+        return crypto.randomUUID();
+    }
+
+    _getOrMakeLine(recordingId, resultIndex) {
+        const recording = this._recordings.find(r => r.id == recordingId);
+        if (!recording) return null;
+        const lineId = `${recordingId}-${resultIndex}`;
+        let line = document.querySelector(`#${lineId}`);
+        if (!line) {
+            line = document.createElement('div');
+            line.id = lineId;
+            line.classList.add('recording-line');
+            recording.appendChild(line);
         }
-        const idValues = this._lines.map(l => l.id);
-        return (Math.max(...idValues) ?? 0) + 1;
+        return line;
     }
 
-    _queueLine(id) {
-        this._outputQueue.push(id);
+    _queueLine(lineId) {
+        this._outputQueue.push(lineId);
         this._playNextLine();
     }
 
@@ -68,20 +90,24 @@ class BrianatorView {
     }
 
     _playNextLine() {
-        if (this._playlingLineId) return;
+        if (this._playingLineId || this._outputQueue.length == 0) return;
         this.speakLine(this._outputQueue.shift());
     }
 
     async speakLine(id, asVoice) {
         if (!id) return;
+        this._playingLineId = id;
         if (!asVoice) { asVoice = 'Brian'; }
 
-        const text = (this._lines.find(l => l.id == id) ?? '').trim();
+        const line = document.getElementById(id);
+        line.classList.add('playing');
+        const text = line.innerText?.trim();
+        if (!text) return this._playNextLine();
     
         const speak = await fetch(`https://api.streamelements.com/kappa/v2/speech?voice=${asVoice}&text=${encodeURIComponent(text)}`);
     
         if (speak.status != 200) {
-            return;
+            return this._playNextLine();
         }
     
         const snd = await speak.blob();
